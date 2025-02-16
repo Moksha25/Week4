85f40c49b30bd94c1f3b0dad57c91b424690d2d7
Mokshini Baglekar <baglekarmokshini03@gmail.com>
2025-02-16 16:39:33 +0530

    delete files

--- Day5-Regex_Junit/src/test/java/JUnit/Basic_JUnit/Testing_Parameterized_Tests/ParameterizedTest.java
+++
@@ -1,4 +0,0 @@
-package JUnit.Basic_JUnit.Testing_Parameterized_Tests;
-
-public @interface ParameterizedTest {
-}
--- Day5-Regex_Junit/src/test/java/JUnit/Basic_JUnit/Testing_Parameterized_Tests/ValueSource.java
+++
@@ -1,5 +0,0 @@
-package JUnit.Basic_JUnit.Testing_Parameterized_Tests;
-
-public @interface ValueSource {
-    int[] ints();
-}
